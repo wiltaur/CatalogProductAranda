5a41b9d8b42994c35ea0539d41053407622a9a03
Wilson Stiven Salgado Blanco <wssalgado@indracompany.com>
2022-06-25 16:51:00 -0500

    Entity Framework como ORM

---
+++ Repository/Model/Category.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace Aranda.CatalogProductCore.Repository.Model
+{
+    [Table("CATEGORY")]
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Required]
+        [Column("NAME")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [InverseProperty(nameof(Product.Category))]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Repository/Model/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace Aranda.CatalogProductCore.Repository.Model
+{
+    [Table("PRODUCT")]
+    public partial class Product
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Required]
+        [Column("NAME")]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Column("DESCRIPTION")]
+        [StringLength(200)]
+        public string Description { get; set; }
+        [Column("CATEGORY_ID")]
+        public int CategoryId { get; set; }
+        [Required]
+        [Column("IMAGE")]
+        public byte[] Image { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        [InverseProperty("Products")]
+        public virtual Category Category { get; set; }
+    }
+}
